115c89af4d8eeeecdea9933d4a3089427a9b116e
Daniel Holmes <daniel.holmes@gmail.com>
2014-08-03 15:54:35 -0500

    Adding a DMS' Meetup Api.  Couldn't find a way to just pull group by url_name so ended up using find.

--- library/routes/api.php
+++ library/routes/api.php
@@ -4,8 +4,29 @@ function ApiRequest(){
     $app = \Slim\Slim::getInstance();
     $app->view(new \JsonApiView());
     $app->add(new \JsonApiMiddleware());
+    $app->container->singleton('meetup', function () use ($app) {
+        $client = DMS\Service\Meetup\MeetupKeyAuthClient::factory(array(
+                'key' => $app->config('meetup.api.key')
+            )
+        );
+        return $client;
+    });
+}
+/** @returns array */
+function getGroupInfo(){
+    $app = \Slim\Slim::getInstance();
+    $response = $app->meetup->getGroups([
+        'group_urlname'=>$app->config('meetup.group.urlname')
+    ]);
+    if($response) {
+        return reset($response);
+    } else {
+        return array();
+    }
 }
 
 $app->get('/api','ApiRequest', function() use ($app) {
-    $app->render(200,['msg'=>'welcome!']);
+    /** @var DMS\Service\Meetup\MeetupKeyAuthClient $client */
+    $groupInfo = getGroupInfo();
+    $app->render(200,$groupInfo);
 });
