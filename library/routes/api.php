511910501c720e150deb9d6f8325366b5d442057
Daniel Holmes <daniel.holmes@gmail.com>
2014-08-16 20:49:01 -0500

    added rsvps for an event - only returns a little info, and only for yes's

--- library/routes/api.php
+++ library/routes/api.php
@@ -18,15 +18,50 @@ function getGroupInfo(){
     $response = $app->meetup->getGroups([
         'group_urlname'=>$app->config('meetup.group.urlname')
     ]);
+
     if($response) {
-        return reset($response);
+        return $response->current();
     } else {
         return array();
     }
 }
 
 $app->get('/api','ApiRequest', function() use ($app) {
-    /** @var DMS\Service\Meetup\MeetupKeyAuthClient $client */
     $groupInfo = getGroupInfo();
     $app->render(200,$groupInfo);
 });
+
+$app->get('/api/events','ApiRequest', function() use ($app) {
+    $status = null;
+    if(@$_GET['status']) {
+        $status = $_GET['status'];
+    }
+
+    $events = $app->meetup->getEvents([
+        'status' => $status,
+        'group_urlname' => $app->config('meetup.group.urlname')
+    ]);
+
+    $app->render(200,$events->toArray());
+});
+
+$app->get('/api/rsvps','ApiRequest', function() use ($app) {
+    $eventId = null;
+    if(@$_GET['event_id']) {
+        $eventId = $_GET['event_id'];
+    }
+
+    $rsvpsRaw = $app->meetup->GetRSVPs([
+        'event_id' => $eventId,
+        'rsvp' => 'yes',
+        'group_urlname' => $app->config('meetup.group.urlname')
+    ]);
+
+    foreach($rsvpsRaw as $rsvp){
+        $rsvps[] = ['member'=>$rsvp['member'],'member_photo'=>$rsvp['member_photo']];
+    }
+
+    $app->render(200,$rsvps);
+});
+
+
