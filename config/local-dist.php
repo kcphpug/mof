31dca77d56878258f48185777839918859ee53c9
Daniel Holmes <daniel.holmes@gmail.com>
2014-09-03 16:41:57 -0500

    Commiting an actual .dist file for the local config file

---
+++ config/local-dist.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Copy this file to local.php and add your meetups url name and api key.
+
+    You will not want to check the local.php file into a public repo.
+*/
+
+return array(
+    'debug' => true,
+    'meetup.api.key' => 'XXX',
+    'meetup.group.urlname'=>'kcphpug'
+);
