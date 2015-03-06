c1c032bd52444cc9f87de29d5faa462648c9b2aa
Daniel Holmes <daniel.holmes@gmail.com>
2015-03-05 18:32:27 -0600

    Forcing / redirect to /wheel for now.

--- library/routes/site.php
+++ library/routes/site.php
@@ -1,14 +1,9 @@
 <?php /** @var $app \Slim\Slim **/
 
 $app->get('/', function () use ($app) {
-    // Sample log message
-    $app->log->info("Slim-Skeleton '/' route");
-    // Render index view
-    $app->render('index.html');
+    $app->redirect('/wheel');
 });
 $app->get('/wheel', function () use ($app) {
-    // Sample log message
     $app->log->info("Slim-Skeleton '/' route");
-    // Render index view
     $app->render('wheel.html');
 });
